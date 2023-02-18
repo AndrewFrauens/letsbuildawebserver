8e498d3bc224c5c99419546bf77b4fea4b5799be
Andrew <AndrewFrauens@users.noreply.github.com>
2023-02-18 00:36:18 -0500

    print out tcp connection content

---
+++ src/bin/rustbook.rs
@@ -0,0 +1,32 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+const IP: &str  = "localhost";
+const PORT: u16 = 8888;
+
+fn main() {
+    let listener = TcpListener::bind(format!("{IP}:{PORT}")).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+}
+
+
+
